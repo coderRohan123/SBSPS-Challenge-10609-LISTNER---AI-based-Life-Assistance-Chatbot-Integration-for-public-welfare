f4cab74a8a03fe705f284635c3d701d256af5f62
Atulya kumar <97295213+atulyakr@users.noreply.github.com>
2023-09-04 15:09:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,58 @@ loaded_model = joblib.load('emotion_model.joblib')
   Shame-7
   surprise-8'''
 
+#telebot
+import telebot
+bot=telebot.TeleBot('6447345913:*************************')
+
+
+@bot.message_handler(commands=['ai','info'])
+def ai_handler(message):
+  if message.text=='/ai':
+    bot.reply_to(message,'Be clear pal. What do you want talk about? 🙂 ')
+  if message.text=='/info':
+    bot.reply_to(message,'Sup!!🙂 i am hearky , your Ai telebot created by Master Atulya.')
+    bot.reply_to(message,'Ask me anything (use /ai followed by your text), will try my best to answer it ✊')
+  else:
+    refine_message=message.text.replace('/ai','')
+    emotion_num=loaded_model.predict([refine_message]) #predicts the sentiment number
+    
+    response=f"""
+    {get_response(refine_message)}
+    """
+    senti_msg=''
+    if(emotion_num==0):
+      senti_msg="you seem angry. I know the whole world sucks sometimes,but we gotta keep our cool and move on."
+      
+  
+    elif(emotion_num==2):
+      alert_info_msg='just be calm and alert and provide your name'
+      bot.reply_to(message,alert_info_msg)
+      victim_name=message.text.replace('/ai','')
+      emergency_msg=f'your friend {victim_name} seems to be in trouble. Please reach out to him asap-- hearky'
+      account_sid = 'ACfb7b5de7cd4934430469c62b5ebf98cc'
+      auth_token = 'e67df05c397e2851b7c06c5e4c7e0ec2'
+      client = Client(account_sid, auth_token)
+      message = client.messages.create(
+                from_='+16184378302',
+                body=emergency_msg,
+                to=num
+               )
+      senti_msg="I have texted the authorities and your family memebers, until they reach out to you just keep calm and keep it low"  
+  
+    elif (emotion_num==6):
+      senti_msg="We all have our lows and highs. These times test us and we need to keep our hearts strong and face it with all of our might.Just believe and everything's gonna be fine ✊"  
+
+    elif (emotion_num==7 or emotion_num==3):
+      senti_msg="There are times when we regret the choices we make, but even then we have our options, whether to keep on thinking about the mistakes or take a choice to somehow mend it. The choices we make at this point makes all the difference my friend :"
+    
+    bot.reply_to(message,response)
+    bot.reply_to(message,senti_msg)
+
+if __name__=='__main__':
+  print('bot is running')
+  bot.polling()
+
 # flask part 
 app=Flask(__name__,template_folder='templates')
 
